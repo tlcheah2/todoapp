e7b7b343888eeb352690c8183a7a90fd16896721
tlcheah2 <tekloon.1991@gmail.com>
2015-06-29 21:49:06 +0800

    Updated coding

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,8 @@ class Item < ActiveRecord::Base
 	validate :due_date_cannot_be_in_the_past
 	
 	def due_date_cannot_be_in_the_past
-		if(self.duedate = Date.today)
+		if(self.duedate < Date.today)
 			errors.add(:duedate,"can't be in the past")
-		end
-	end
-	
-	def overdue
-		if self.duedate < Time.now
-			return true;
-		else
-			return false;
 		end
 	end
 end
