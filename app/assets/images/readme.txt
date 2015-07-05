e480885ba9311d30148b589900a75c1a6ea13902
Tek Loon Cheah <tekloon.1991@gmail.com>
2015-07-05 13:24:26 +0800

    updated interface

---
+++ app/assets/images/readme.txt
@@ -0,0 +1,7 @@
+1. install genymotion-2.5.0-vbox.exe
+2. open the genymotion
+3. sign up an genymotion account and login
+4. copy genymotion_vbox86tp_5.1_150610_103034.ova to directory
+	C:\Users\<your_PC_user_name>\AppData\Local\Genymobile\Genymotion\ova
+5. Add -> select android version 5.1.0 -> select Google Nexus 7 - 5.1.0 - API 22 -> Next -> OK
+6. Start the added virtual devices 
---
+++ hello.rb
@@ -0,0 +1,23 @@
+print "Please enter your first name "
+name = gets
+website =  <<-STRING
+Hello
+Diu nia lao mei
+hahaha
+STRING
+puts "Hello #{name}!"
+puts 'Hello #{name}'
+puts "Hello #{website}!"
+
+a = 10
+b = 10.32
+c = a + b;
+d = 2 ** 8
+puts "In fact, the result of a + b is #[c]"
+
+# Print out the number adding together and return the sum value
+#
+def add(a,b)
+	puts "Adding of #{a} and #{b} is:"
+	
+end
